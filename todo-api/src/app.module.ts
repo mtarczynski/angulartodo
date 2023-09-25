fda5ef3e94f1bd474b8a67f06f17d6dc3169e51b
Maciej Tarczyski <m.tarczynski97@gmail.com>
2023-09-25 06:49:26 +0200

    Create Authentication API

--- todo-api/src/app.module.ts
+++ todo-api/src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     MongooseModule.forRoot("mongodb+srv://mtarczynski:SlNz5ZCNgCPBk1hW@cluster0.s8olgak.mongodb.net/todoDB?retryWrites=true&w=majority"),
-    TodoModule
+    TodoModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
